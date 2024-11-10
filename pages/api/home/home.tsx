922fcabcc74726c584d5c2f92dec6317d218b796
adityasugandhi <adityasugandhi@artemis.sc.fsu.edu>
2024-11-09 19:03:41 -0500

    prod ready

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -303,7 +303,7 @@ const Home = ({ defaultModelId }: Props) => {
       }}
     >
       <Head>
-        <title>Chatbot Ollama</title>
+        <title>My ChatBot</title>
         <meta name="description" content="ChatGPT but local." />
         <meta
           name="viewport"
