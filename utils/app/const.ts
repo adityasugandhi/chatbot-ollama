bd6306f664a800d02b9b4fa3e029990462a19c74
adityasugandhi <adityasugandhi@artemis.sc.fsu.edu>
2024-11-09 20:11:42 -0500

    changes

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -2,8 +2,7 @@ export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
   "";
 
-export const OLLAMA_HOST =
-  process.env.OLLAMA_HOST || 'http://egret-perfect-dane.ngrok-free.app';
+export const OLLAMA_HOST =  process.env.OLLAMA_HOST;
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
